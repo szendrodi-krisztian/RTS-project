e1aa6c4dc70763c396c218f17767399d8995eba4
szend <szend@DESKTOP-3U3918F>
2016-02-16 22:49:01 +0100

    Menu

--- src/mygame/MainMenuState.java
+++ src/mygame/MainMenuState.java
@@ -7,16 +7,20 @@ import com.jme3.app.state.AppStateManager;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.scene.Node;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
 
 /**
  *
  * @author szend
  */
-public class MainMenuState extends AbstractAppState {
+public class MainMenuState extends AbstractAppState implements ScreenController {
 
     private SimpleApplication app;
-    
+
     private Node menuNode;
 
     @Override
@@ -30,11 +34,15 @@ public class MainMenuState extends AbstractAppState {
 
     }
 
+    private NiftyJmeDisplay niftyDisplay;
+
     @Override
     public void setEnabled(boolean enabled) {
         super.setEnabled(enabled);
         if (enabled) {
             app.getGuiNode().attachChild(menuNode);
+            niftyDisplay.getNifty().gotoScreen("start");
+            app.getFlyByCamera().setEnabled(false);
             ActionListener al = new ActionListener() {
 
                 @Override
@@ -50,6 +58,7 @@ public class MainMenuState extends AbstractAppState {
             app.getInputManager().addMapping("to the battle", new KeyTrigger(KeyInput.KEY_N));
             app.getInputManager().addListener(al, new String[]{"to the battle"});
         } else {
+            niftyDisplay.getNifty().gotoScreen("ingame");
             app.getInputManager().clearMappings();
             app.getGuiNode().detachChild(menuNode);
         }
@@ -60,8 +69,32 @@ public class MainMenuState extends AbstractAppState {
         super.initialize(stateManager, app);
         this.app = (SimpleApplication) app;
         menuNode = new Node("Menu Node");
-        System.out.println((int)(1%2));
+        niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(app.getAssetManager(), app.getInputManager(), app.getAudioRenderer(), app.getGuiViewPort());
+        Nifty nifty = niftyDisplay.getNifty();
+        nifty.fromXml("Interface/menuGui.xml", "start", this);
+        app.getGuiViewPort().addProcessor(niftyDisplay);
         setEnabled(true);
+    }
+
+    public void toTheBattleField() {
+
+        setEnabled(false);
+        app.getStateManager().getState(BattleState.class).setEnabled(true);
+    }
+
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+        nifty.gotoScreen("start");
+    }
+
+    @Override
+    public void onStartScreen() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onEndScreen() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
