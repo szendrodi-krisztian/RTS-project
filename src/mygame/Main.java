97e685363b5fa64b91990ecefbf941ff2f1a6017
szend <szend@DESKTOP-3U3918F>
2016-02-19 16:51:18 +0100

    Refactoring some stuff

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,12 +1,10 @@
 package mygame;
 
-import battle.BattleMap;
 import com.jme3.app.SimpleApplication;
 import com.jme3.renderer.RenderManager;
 
 public class Main extends SimpleApplication {
 
-    BattleMap map;
 
     public static void main(String[] args) {
         Main app = new Main();
